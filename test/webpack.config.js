b9363237c517c07381054e543df98b9e433bf2a1
Kaito Udagawa <umireon@gmail.com>
2016-12-01 13:16:20 +0900

    style(standard): fix style

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -7,14 +7,14 @@ module.exports = {
   },
   plugins: [
     new webpack.DefinePlugin({
-      'process.browser': true,
+      'process.browser': true
     }),
     new webpack.ProvidePlugin({
       setTimeout: path.join(__dirname, '/jxa-timeout/setTimeout'),
       clearTimeout: path.join(__dirname, '/jxa-timeout/clearTimeout'),
       setInterval: path.join(__dirname, '/jxa-timeout/setInterval'),
       clearInterval: path.join(__dirname, '/jxa-timeout/clearInterval')
-    }),
+    })
   ],
   node: {
     fs: 'empty'

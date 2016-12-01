1941efb685aa859498b8c6f25f99face840377e3
Kaito Udagawa <umireon@gmail.com>
2016-12-01 18:23:15 +0900

    docs: add install-osa.sh

---
+++ install-osa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER=umireon
+NAME=editorconfig-jxa
+
+INSTALL_DIR=${INSTALL_DIR-~/Library/Script\ Libraries}
+
+if [[ $1 == "" ]]; then
+  TAG=$(curl -fsIL https://github.com/$USER/$NAME/releases/latest -o /dev/null -w '%{url_effective}')
+  TAG=${TAG##*/}
+else
+  TAG="$1"
+fi
+
+echo "installing $INSTALL_DIR/$NAME.scpt ..."
+curl -fL "https://github.com/$USER/$NAME/releases/download/$TAG/$NAME.scpt" -o "$INSTALL_DIR/$NAME.scpt"
+
+echo "$NAME is installed"
+echo "use this library with:"
+echo "  Library('$NAME')"

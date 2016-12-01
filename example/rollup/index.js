566b62e86db6c029f444a6aea0922e81487cda8d
Kaito Udagawa <umireon@gmail.com>
2016-12-02 02:45:24 +0900

    doc: remove examples

--- example/rollup/index.js
+++
@@ -1,7 +0,0 @@
-import { parseSync } from 'editorconfig-jxa'
-import * as chai from 'chai'
-
-chai.assert.deepEqual(
-  parseSync('../../test/fixture/test.js'),
-  { indent_size: 4, tab_width: 4, indent_style: 'space' }
-)
--- example/webpack/index.js
+++
@@ -1,7 +0,0 @@
-var editorconfig = require('editorconfig-jxa')
-var assert = require('assert')
-
-assert.deepEqual(
-  editorconfig.parseSync('../../test/fixture/test.js'),
-  { indent_size: 4, tab_width: 4, indent_style: 'space' }
-)
